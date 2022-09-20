7116fd40d7a941dcd2df4365b9ea2a1f3148eac3
Marcelo Heredia <heredia_m@outlook.com>
2022-09-19 23:50:44 -0300

    changes

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import random
 import peer
-import superpeer
+import super_peer
 import json
+import sys
+from util import parameter_error_message, display_help
 
 configs = dict()
 super_peer_list = []
 
 
 def main():
-    read_config()
-    create_peer()
+    print(sys.argv)
+
+    if len(sys.argv) <= 1:
+        parameter_error_message()
+        return
+    filter_args()
+
+
+def filter_args():
+    match sys.argv[1]:
+        case '-h':
+            display_help()
+            return
+        case '-sp':
+            read_config()
+            create_super_peer()
+        case '-p':
+            create_peer()
+        case _:
+            parameter_error_message()
 
 
 def create_peer():
+    if len(sys.argv) < 3:
+        parameter_error_message()
+    pass
+
+
+def create_super_peer():
     global super_peer_list
-    superpeer_number = 0
-    while True:
-        superpeer_number = int(input("Numero de super nodes:"))
-        if 0 < superpeer_number <= len(configs):
-            break
-        else:
-            print("Numero invalido")
     for index, peer_name in enumerate(configs):
-        if index <= superpeer_number - 1:
-            superpeer.SuperPeer(peer_name, get_config(index, "addr"), get_config(index, "port"),
-                                get_config(index, "next"))
-            super_peer_list.append((get_config(index, "addr"), get_config(index, "port")))
-        else:
-            peer.Peer(peer_name, get_config(index, "addr"), get_config(index, "port"), get_config(index, "next"),
-                      super_peer_list[random.randint(0, len(super_peer_list) - 1)])
+        super_peer.SuperPeer(peer_name, get_config(index, 'addr'), get_config(index, 'port'),
+                            get_config(index, 'next'))
+        super_peer_list.append((get_config(index, 'addr'), get_config(index, 'port')))
 
 
 def read_config():
     global configs
-    file = open("config.json")
+    file = open('config.json')
     configs = json.loads(file.read())
 
 
---
+++ util.py
@@ -0,0 +1,11 @@
+
+
+def display_help():
+    print('to execute in Super-Peer mode, run with:')
+    print('\t\t python program.py -sp')
+    print('to execute in peer mode, run with:')
+    print('\t\t python program.py -p <ip> <port>')
+
+
+def parameter_error_message():
+    print('Error: incorrect parameters. Run with -h for support')

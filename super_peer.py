98eb56ea53f4092b42ceb781ef749a1135bacd66
Lucassilv2 <lucas.032@acad.pucrs.br>
2022-09-20 21:20:12 -0300

    logica da tabela

--- super_peer.py
+++ super_peer.py
@@ -1,3 +1,4 @@
+import json
 import socket
 import threading
 import time
@@ -7,6 +8,7 @@ class SuperPeer:
 
     def __init__(self, name, addr, port, next_peer):
         self.connected_peers: list[(str, int, int)] = list()
+        self.table: dict[(str, int), list[str]] = dict()
         self.addr = addr
         self.name = name
         self.port = port
@@ -17,15 +19,36 @@ class SuperPeer:
         self.receive_message_thread = threading.Thread(target=self.receive_message)
         self.receive_message_thread.start()
         print(self.name)
-        self.connected_peers.append(('10.1.1.1', 5000, time.time()))
-        self.clear_peer_list()
+        #self.connected_peers.append(('10.1.1.1', 5000, time.time()))
+        self.clear_peer_list_thread = threading.Thread(target=self.clear_peer_list)
+        self.clear_peer_list_thread.start()
 
     def receive_message(self):
         while True:
             data, addr = self.socket.recvfrom(1024)
-            data_str = data.decode('utf-8')
-            print(self.name, data_str, addr)
+            data_json = json.loads(data.decode('utf-8'))
+            if data_json['type'] == 1:
+                self.connected_peers.append((addr[0], addr[1], time.time()))
+                data_to_remove = list()
 
+                if (addr[0], addr[1]) not in self.table:
+                    self.table[(addr[0], addr[1])] = list()
+
+                for k, v in data_json['contents'].items():
+                    self.table[(addr[0], addr[1])].append(v)
+                    data_to_remove.append(k)
+
+                for k in data_to_remove:
+                    data_json['contents'].pop(k)
+                
+                print(data_json['contents'])
+                
+                if data_json['contents'] != None:
+                    data_json['type'] = 2
+                    #self.send_message_for_next(json.dumps(data_json), self.next_peer)
+
+                    
+            
             # DECODE RECEIVED PACKET JSON
 
     def clear_peer_list(self):
@@ -43,4 +66,9 @@ class SuperPeer:
             time.sleep(1)
 
     def clear_peer_hashes(self, peer):
-        pass
+        self.table.pop((peer[0], peer[1]), None)
+        print('tabela limpa ->',self.table)
+        print('peer limpo ->',self.connected_peers)
+
+    def send_message_for_next(self, message, addr):
+        self.socket.sendto(message.encode(), addr)
